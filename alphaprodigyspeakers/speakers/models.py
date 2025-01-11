a1d522f821c372d3dd65f48caf1cfd267423948c
johCodes <johcodes19@gmail.com>
2025-01-11 07:24:33 +0300

    I have set up the django environment for the web app. I have also created models for the same.

---
+++ alphaprodigyspeakers/speakers/models.py
@@ -0,0 +1,20 @@
+# bookings/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    booked_on = models.DateTimeField(auto_now_add=True)
+
+class Order(models.Model):
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
