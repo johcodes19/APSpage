ea4a6b75896546bcf58b655ee846f6af1f0fa0c2
johCodes <johcodes19@gmail.com>
2025-01-11 11:33:48 +0300

    registered the models in admin.py

--- alphaprodigyspeakers/speakers/admin.py
+++ alphaprodigyspeakers/speakers/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Service, Booking, Order
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    search_fields = ('name',)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'service', 'date', 'booked_on')
+    list_filter = ('service', 'date')
+    search_fields = ('user__username',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('booking', 'created_at', 'is_paid')
+    list_filter = ('is_paid',)
