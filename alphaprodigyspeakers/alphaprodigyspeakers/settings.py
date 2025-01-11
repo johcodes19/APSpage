b17371e74f6ad87562126642ffa82e3c6b488e1e
johCodes <johcodes19@gmail.com>
2025-01-11 13:14:24 +0300

    updated css file.

--- alphaprodigyspeakers/alphaprodigyspeakers/settings.py
+++ alphaprodigyspeakers/alphaprodigyspeakers/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+
 ROOT_URLCONF = 'alphaprodigyspeakers.urls'
 
 TEMPLATES = [
